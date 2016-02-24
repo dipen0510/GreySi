043f0f9be92d2653dd109b7c3b3ab741a733b3c0
Dipen Sekhsaria <dipen_sekhsaria@infosys.com>
2016-02-24 17:09:46 +0530

    Initial Commit

---
+++ GreySi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreySi
+//
+//  Created by Dipen Sekhsaria on 24/02/16.
+//  Copyright © 2016 GreyScissors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreySi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GreySi
+//
+//  Created by Dipen Sekhsaria on 24/02/16.
+//  Copyright © 2016 GreyScissors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
